aa16e965f0fa6b146543756beedf218dddec8cd0
src1138 <src1138@gmail.com>
2023-09-13 02:43:21 +0200

    split logging from monitor

--- VMFB_logger.sh
+++ VMFB_logger.sh
@@ -6,8 +6,6 @@ PIR=27
 SIR=17
 TMR=22
 EMT=23
-DEP=26
-DIS=20
  
 # Verify they are set up, else initialize them
 test -e /sys/class/gpio/gpio$PIR ||
@@ -16,12 +14,6 @@ test -e /sys/class/gpio/gpio$PIR ||
 test -e /sys/class/gpio/gpio$SIR || 
   (echo $SIR > /sys/class/gpio/export \
    && echo in > /sys/class/gpio/gpio$SIR/direction)
-test -e /sys/class/gpio/gpio$DEP ||
-  (echo $DEP > /sys/class/gpio/export \
-   && echo in > /sys/class/gpio/gpio$DEP/direction)
-test -e /sys/class/gpio/gpio$DIS ||
-  (echo $DIS > /sys/class/gpio/export \
-   && echo in > /sys/class/gpio/gpio$DIS/direction)
 test -e /sys/class/gpio/gpio$TMR || 
   (echo $TMR > /sys/class/gpio/export \
    && echo in > /sys/class/gpio/gpio$TMR/direction)
@@ -29,7 +21,13 @@ test -e /sys/class/gpio/gpio$EMT ||
   (echo $EMT > /sys/class/gpio/export \
    && echo in > /sys/class/gpio/gpio$EMT/direction)
 
+while true
+do
+
 # Get the values for the last script execution
+prev_valPIR=$(cat /data/log/prev_valPIR)
+prev_valSIR=$(cat /data/log/prev_valSIR)
+prev_valTMR=$(cat /data/log/prev_valTMR)
 prev_valEMT=$(cat /data/log/prev_valEMT)
 
 # Get the current pin values
@@ -42,8 +40,15 @@ if [ "$valSIR" == "0" ]; then
 else
     valEMT=$prev_valEMT
 fi
-valDEP=$(cat /sys/class/gpio/gpio$DEP/value)
-valDIS=$(cat /sys/class/gpio/gpio$DIS/value)
+
+# Update the previous values to the current values
+echo $valPIR >| /data/log/prev_valPIR
+echo $valSIR >| /data/log/prev_valSIR
+echo $valTMR >| /data/log/prev_valTMR
+# Only update the EMT previous value if the sensors are on
+if [ "$valSIR" == "0" ]; then
+    echo $valEMT >| /data/log/prev_valEMT
+fi
 
 # Set the display text for the pin values 
 
@@ -55,8 +60,16 @@ fi
  
 if [ "$valSIR" == "1" ]; then
     SIR_text="-"
+    if [ "$valSIR" != "$prev_valSIR" ]; then
+        curl  http://localhost:7999/1/detection/pause
+        curl http://localhost:7999/1/action/eventend
+    fi
 else
     SIR_text="+"
+    if [ "$valSIR" != "$prev_valSIR" ]; then
+        curl  http://localhost:7999/1/detection/start
+        curl http://localhost:7999/1/action/eventstart
+    fi
 fi
 
 if [ "$valTMR" == "1" ]; then
@@ -71,25 +84,29 @@ else
     EMT_text="+"
 fi
 
-# Deposit and Dispense events are too short to catch unless there is a jam
-if [ "$valDIS" == "0" ]; then
-    DIS_text="+"
-else
-    DIS_text="-"
-fi
-
-if [ "$valDEP" == "0" ]; then
-    DEP_text="+"
-else
-    DEP_text="-"
-fi
-
+# Log any values that changed since the last execution
+dtStamp=$(date +%F_%X)
 VMFB_logfile="/data/log/VMFB_$(date +%F).log"
-PIRcount=$(grep PIR $VMFB_logfile | grep -c "+")
-DEPcount=$(grep DEP $VMFB_logfile | grep -c "+")
-DIScount=$(grep DIS $VMFB_logfile | grep -c "+")
+touch "$VMFB_logfile"
 
-# Output to the UI
-echo "PIR$PIR_text$PIRcount|IR$SIR_text|TMR$TMR_text|FEED$EMT_text|DEP$DEP_text$DEPcount|DIS$DIS_text$DIScount"
-# 1 second delay between executions
-echo 1 1>&2
+if [ "$valPIR" != "$prev_valPIR" ]; then
+    echo "$dtStamp	PIR	$valPIR	$PIR_text">> "$VMFB_logfile"
+fi
+
+if [ "$valSIR" != "$prev_valSIR" ]; then
+    echo "$dtStamp	IR	$valSIR	$SIR_text">> "$VMFB_logfile"
+fi
+
+if [ "$valTMR" != "$prev_valTMR" ]; then
+    echo "$dtStamp	TMR	$valTMR	$TMR_text">> "$VMFB_logfile"
+fi
+
+if [ "$valEMT" != "$prev_valEMT" ]; then
+    if [ "$valSIR" == "0" ]; then
+        echo "$dtStamp	EMT	$valEMT	$EMT_text">> "$VMFB_logfile"
+    fi
+fi
+
+sleep 1
+
+done
