538d37f84235049a99e41563c2eccd614842e56b
src1138 <src1138@gmail.com>
2023-09-14 02:09:08 +0200

    tweaks

--- userinit.sh
+++ userinit.sh
@@ -2,3 +2,5 @@
 /usr/bin/tvservice -o >> /data/log/userinit.log
 # Start deposit/dispense logger
 nohup /data/etc/VMFB_logger.py >> /data/log/userinit.log &
+# Start logger for PIR, Sensor IR, Timer and Empty sensors
+nohup /data/etc/VMFB_logger.sh >> /data/log/userinit.log &
